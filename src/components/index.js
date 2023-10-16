3862c95a5a337ccd04f9cf1b15a66270857cd7fb
Georgi.Yankov <georgi.qnkov@gmail.com>
2023-10-16 23:48:26 +0300

    .

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,10 @@
-import NvBtn from "./nv-button/NvBtn.vue";
-import NvEditor from "./nv-editor";
+import NvBtn from './nv-button/NvBtn.vue';
+import NvEditor from './nv-editor/NvEditor.vue';
 
 import NvExpandTile from './nv-expand-tile/NvExpandTile.vue';
-import NvLoadingBar from "./nv-loading-bar";
-import NvModalPanel from "./nv-modal-panel";
-import NvNoticeBox from "./nv-notice-box";
+import NvLoadingBar from './nv-loading-bar/NvLoadingBar.vue';
+import NvModalPanel from './nv-modal-panel/NvModalPanel.vue';
+import NvNoticeBox from './nv-notice-box/NvNoticeBox.vue';
 
 export default {
   NvBtn,
